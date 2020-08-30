8104da1127ef8852330646cee79ef4bf119fc930
Jhon <jhon.1620@live.com>
2020-08-30 12:17:29 -0500

    .

--- rutas/cita/index.js
+++ rutas/cita/index.js
@@ -3,6 +3,36 @@ const { pool } = require('../../connection/connection')
 
 const app = express()
 
+app.get('/cita/:id', async (req, res) => {
+
+  const con = await pool.connect()
+
+  try {
+
+    const reply = await con.query(`SELECT c.*, d.NOMBRES_APELLIDOS as NOMBRE_DUENIO, d.email, d.dni, d.telefono,
+    m.NOMBRE as NOMBRE_MASCOTA, m.date_creation as fechaRegistroMascota, doc.NOMBRES_APELLIDOS as NOMBRE_DOCTOR,
+    e.DESCRIPTION as estado, rm.DESCRIPTION as raza, tm.DESCRIPTION as tipo
+    FROM cita c INNER JOIN duenio d ON 
+    c.id_duenio = d.id_duenio INNER JOIN mascota m ON
+    c.id_mascota = m.id_mascota INNER JOIN doctor doc ON
+    c.id_doctor = doc.id_doctor INNER JOIN estado e ON
+    c.id_estado = e.id_estado INNER JOIN razamascota rm ON
+    m.raza = rm.id_raza INNER JOIN tipomascota tm ON
+    m.tipo = tm.id_tipo 
+    WHERE fecha_salida IS NULL AND id_cita = ${req.params.id};`)
+
+    res.json({
+      reply: reply.rows,
+      rowCount: reply.rowCount,
+    })
+
+  } catch (e) {
+    throw new Error(e)
+  }finally {
+    con.release()
+  }
+})
+
 app.get('/cita', async (req, res) => {
 
   const con = await pool.connect()
--- rutas/historial/index.js
+++ rutas/historial/index.js
@@ -3,6 +3,34 @@ const { pool } = require('../../connection/connection')
 
 const app = express()
 
+app.get('/historial/:id', async (req, res) => {
+
+  const con = await pool.connect()
+
+  try {
+    const reply = await con.query(`SELECT c.*, d.NOMBRES_APELLIDOS as NOMBRE_DUENIO, d.email, d.dni, d.telefono,
+    m.NOMBRE as NOMBRE_MASCOTA, m.date_creation as fechaRegistroMascota, doc.NOMBRES_APELLIDOS as NOMBRE_DOCTOR,
+    e.DESCRIPTION as estado, rm.DESCRIPTION as raza, tm.DESCRIPTION as tipo
+    FROM cita c INNER JOIN duenio d ON 
+    c.id_duenio = d.id_duenio INNER JOIN mascota m ON
+    c.id_mascota = m.id_mascota INNER JOIN doctor doc ON
+    c.id_doctor = doc.id_doctor INNER JOIN estado e ON
+    c.id_estado = e.id_estado INNER JOIN razamascota rm ON
+    m.raza = rm.id_raza INNER JOIN tipomascota tm ON
+    m.tipo = tm.id_tipo 
+    WHERE fecha_salida IS NOT NULL AND id_cita = ${req.params.id};`)
+
+    res.json({
+      reply: reply.rows,
+      rowCount: reply.rowCount,
+    })
+  } catch (error) {
+    throw new Error(error)
+  } finally {
+    con.release()
+  }
+})
+
 app.get('/historial', async (req, res) => {
 
   const con = await pool.connect()
